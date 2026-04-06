464d82ab1908f285a1ca383b18e0f3c89bb3c724
Ignacio Garcia <i.garcia@hotelbeds.com>
2026-04-06 20:18:43 +0200

    format

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,1 +1,24 @@
-<template><header class="menu"><strong>Artista</strong><nav><template v-for="i in menu" :key="i.label"><router-link v-if="!i.children" :to="i.to">{{i.label}}</router-link><div v-else class="submenu"><span>{{i.label}}</span><div class="submenu-items"><router-link v-for="c in i.children" :key="c.label" :to="c.to">{{c.label}}</router-link></div></div></template></nav></header></template><script setup>import{ref,onMounted}from'vue';import{useMenu}from'@/utils/useMenu';const menu=ref([]);onMounted(async()=>menu.value=await useMenu())</script>
+<template>
+  <header class="menu">
+    <strong>Artista</strong>
+    <nav>
+      <template v-for="i in menu" :key="i.label"
+        ><router-link v-if="!i.children" :to="i.to">{{ i.label }}</router-link>
+        <div v-else class="submenu">
+          <span>{{ i.label }}</span>
+          <div class="submenu-items">
+            <router-link v-for="c in i.children" :key="c.label" :to="c.to">{{
+              c.label
+            }}</router-link>
+          </div>
+        </div></template
+      >
+    </nav>
+  </header>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useMenu } from "@/utils/useMenu";
+const menu = ref([]);
+onMounted(async () => (menu.value = await useMenu()));
+</script>
